5d47fd7cd80c9e3d28c72ed8043181bcda78c18e
denizerkin <erkinkasapli@gmail.com>
2023-10-28 16:31:23 +0300

    weighted knn is added

--- knn.py
+++ knn.py
@@ -2,9 +2,10 @@ import numpy as np
 
 class KNN:
 
-  def __init__(self, k=3):
+  def __init__(self, k=3, weight=None):
     self.k = k
-
+    self.weight = weight
+    
   def euclidean_distance(self, x, y):
     distance = np.sqrt(np.sum(x-y)**2)
     return distance
@@ -26,16 +27,27 @@ class KNN:
       distance = self.euclidean_distance(x,train)
       distances.append(distance)
     min_distance_indices = sorted(range(len(distances)), key=lambda index: distances[index])[:self.k]
+    min_distances = sorted(distance)[:self.k]
     print(min_distance_indices)
     min_distance_labels = []
     for i in min_distance_indices:
       min_distance_labels.append(self.y_train[i])
-
-    most_common = {}
-    for i in min_distance_labels:
-      if i not in most_common.keys():
-        most_common[i] = 1
-      else:
-        most_common[i] += 1
-    return max(most_common, key = lambda x:most_common[x])
+    
+    if self.weight == 'distance':
+      most_common = {}
+      for i in range(len(min_distance_labels)):
+        current_label = min_distance_labels[i]
+        if current_label not in most_common.keys():
+          most_common[current_label] = 1/min_distances[i]
+        else:
+          most_common[current_label] += 1/min_distances[i]
+      return max(most_common, key = lambda x:most_common[x])
+    else:
+      most_common = {}
+      for i in min_distance_labels:
+        if i not in most_common.keys():
+          most_common[i] = 1
+        else:
+          most_common[i] += 1
+      return max(most_common, key = lambda x:most_common[x])
 
